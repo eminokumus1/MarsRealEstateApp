05245d6a4dafe43960760257d6dcfc082f591ae1
eminokumus1 <eminokumus1@gmail.com>
2024-05-30 20:35:35 +0300

    Mars properties retrieved and converted from Json to MarsProperties

---
+++ app/src/main/java/com/eminokumus/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.eminokumus.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.eminokumus.marsrealestate.network.MarsProperty
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+import com.eminokumus.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Response
+
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _response = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    /**
+     * Gets filtered Mars real estate property information from the Mars API Retrofit service and
+     * updates the [MarsProperty] [List] and [MarsApiStatus] [LiveData]. The Retrofit service
+     * returns a coroutine Deferred, which we await to get the result of the transaction.
+     * @param filter the [MarsApiFilter] that is sent as part of the web server request
+     */
+    private fun getMarsRealEstateProperties() {
+        MarsApi.retrofitService.getProperties().enqueue(object : retrofit2.Callback<List<MarsProperty>> {
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success: ${response.body()?.size} Mars properties retrieved."
+            }
+
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+        })
+        _response.value = "Set the Mars API Response here!"
+    }
+}
